4e061f3d0429c88a7f4d8c8659307df13926bf83
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2021-08-17 23:43:01 +0530

    added func to convert number into string

--- index.js
+++ index.js
@@ -17,3 +17,26 @@ function reverseString(str){
     return str===reverseStr
  
  }
+
+ function convertDateToString(date){
+    let dateStr={
+          day:"",
+          month:"",
+          year:""
+      }
+   if(date.day<10){
+       dateStr.day="0"+date.day;
+   }else{
+       dateStr.day=date.day.toString();
+   }
+  
+   if(date.month<10){
+       dateStr.month="0"+date.month;
+   }else{
+       dateStr.month= date.month.toString();
+   }
+  
+   dateStr.year=date.year.toString();
+   return dateStr  
+  
+  }
