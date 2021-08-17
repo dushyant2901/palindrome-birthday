7c65f91a8baac0d6ab45715aec612c4e83167f52
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2021-08-17 23:41:13 +0530

    added function to reverse string

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const dateInputRef=document.querySelector("#bday-input");
 const showBtnRef=document.querySelector("#show-btn");
 const resultRef=document.querySelector("#result");
 
-showBtnRef.addEventListener("click",clickHandler)
+showBtnRef.addEventListener("click",clickHandler);
+
+function reverseString(str){
+    let listOfChar=str.split("");
+   let reverseListOfChar=listOfChar.reverse();
+   let reversedString=reverseListOfChar.join("");
+   return reversedString 
+  // return str.split("").reverse().join("")
+ } 
