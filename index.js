8fe80a437013a712e7f86d13721fb6a00846e498
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2021-08-17 23:39:27 +0530

    added eventlistner

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const dateInputRef=document.querySelector("#bday-input");
 const showBtnRef=document.querySelector("#show-btn");
 const resultRef=document.querySelector("#result");
+
+showBtnRef.addEventListener("click",clickHandler)
